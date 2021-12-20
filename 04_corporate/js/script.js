5ea910a1e3072f642e44c8fca7b69113ef9b0268
paraizo2424 <e095745@ie.u-ryukyu.ac.jp>
2021-12-21 07:42:45 +0900

    練習問題の回答：円グラフを表示

--- 04_corporate/js/script.js
+++ 04_corporate/js/script.js
@@ -40,3 +40,14 @@ new Chartist.Bar('.bar-chart', barData, barOptions);
 - 猫 35%、犬 30%、きりん 20%、その他 15%
 - 円グラフのオプションには 幅100%、高さ300px を指定
 */
+var animalData = {
+  labels: ['猫', '犬', 'きりん', 'その他'],
+  series: [35, 30, 20, 15]
+};
+
+var animalOptions = {
+  width: '100%',
+  height: '300px'
+};
+
+new Chartist.Pie('.animal-chart', animalData, animalOptions);
