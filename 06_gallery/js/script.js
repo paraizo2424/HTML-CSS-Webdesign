0fabb651a42c2f03d08a4d7b2acd1d7a4106ce25
paraizo2424 <e095745@ie.u-ryukyu.ac.jp>
2022-02-13 15:27:47 +0900

    画像のキャプションを表示する

--- 06_gallery/js/script.js
+++ 06_gallery/js/script.js
@@ -1,1 +1,8 @@
-new LuminousGallery(document.querySelectorAll(".grid-gallery"));
+// new LuminousGallery(document.querySelectorAll(".grid-gallery"));
+
+// 画像のキャプションを表示する
+new LuminousGallery(document.querySelectorAll(".grid-gallery"),{},{
+  caption: function(trigger){
+    return trigger.querySelector('img').getAttribute('alt');
+  }
+});
